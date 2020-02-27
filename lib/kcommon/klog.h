3a4e835c22a75307ed38300aa14751e5ec94fda3
KH <kh@kh>
2020-02-26 21:07:53 -0600

    logging

---
+++ lib/kcommon/klog.h
@@ -0,0 +1,14 @@
+//
+// Created by KH on 2/26/2020.
+//
+
+#ifndef IGEN3_KLOG_H
+#define IGEN3_KLOG_H
+
+#include <easylogging++.h>
+#include <fmt/format.h>
+
+#define LOG(level, f, ...) ELPP_LOG(level) << fmt::format(f, ##__VA_ARGS__)
+#define LOG_IF(condition, level, f, ...) ELPP_LOG_IF(condition, level) << fmt::format(f, ##__VA_ARGS__)
+
+#endif //IGEN3_KLOG_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <klog.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+INITIALIZE_EASYLOGGINGPP
+
+int main(int argc, char *argv[]) {
+    START_EASYLOGGINGPP(argc, argv);
+    el::Configurations conf("logging.conf");
+    el::Loggers::reconfigureAllLoggers(conf);
+
+    LOG_IF(1 < 2, INFO, "ads") << "  asd";
+
     return 0;
 }
